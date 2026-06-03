25067cf8be927bda11540503beb93c004e1eaf96
Любодеева Ирина <ILyubodeeva@alfabank.ru>
2026-06-03 20:20:41 +0300

    Домашнее задание 2

---
+++ Lesson2/Main.kt
@@ -0,0 +1,83 @@
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONArray
+import org.json.JSONObject
+import java.util.concurrent.TimeUnit
+
+data class GenerationConfig(
+    val temperature: Double = 1.0,
+    val topP: Double = 1.0,
+    val maxTokens: Int = 200,
+    val stop: List<String> = emptyList()
+)
+
+class DeepSeekClient(private val apiKey: String) {
+
+    val client = OkHttpClient.Builder()
+        .connectTimeout(10, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .build()
+    private val mediaType = "application/json".toMediaType()
+
+    fun ask(
+        prompt: String,
+        config: GenerationConfig = GenerationConfig()
+    ): String {
+
+        val requestBody = JSONObject()
+            .put("model", "deepseek-v4-flash")
+            .put(
+                "messages",
+                JSONArray().put(
+                    JSONObject()
+                        .put("role", "user")
+                        .put("content", prompt)
+                )
+            )
+            .put("temperature", config.temperature)
+            .put("top_p", config.topP)
+            .put("max_tokens", config.maxTokens)
+            .put("stop", JSONArray(config.stop))
+
+        val request = Request.Builder()
+            .url("https://api.deepseek.com/v1/chat/completions")
+            .header("Authorization", "Bearer $apiKey")
+            .post(requestBody.toString().toRequestBody(mediaType))
+            .build()
+
+        client.newCall(request).execute().use { response ->
+
+            if (!response.isSuccessful) {
+                error("Ошибка API: ${response.code}")
+            }
+
+            val json = JSONObject(response.body!!.string())
+
+            return json.getJSONArray("choices")
+                .getJSONObject(0)
+                .getJSONObject("message")
+                .getString("content")
+        }
+    }
+}
+
+fun main() {
+    val apiKey = System.getenv("DEEPSEEK_API_KEY") ?: error("API key not found")
+
+    val llm = DeepSeekClient(apiKey)
+    val prompt = "Расскажи о Kotlin в 2–3 коротких предложениях."
+
+    val config = GenerationConfig(
+        temperature = 0.2,
+        topP = 0.8,
+        maxTokens = 300,
+        stop = listOf(".")
+    )
+
+    val answer = llm.ask(prompt, config)
+
+    println(answer)
+}
